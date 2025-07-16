5dc19b5cce343e0682b182152fe665598fb414e0
Yulia Chirkova <yu.ya98@ya.ru>
2025-07-16 20:32:49 +0300

    добавлено открытие меню на десктопе, создана мобильная версия

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const catalogOpener = document.querySelector('.menu-catalog-opener');
     const whoOpener = document.querySelector('.menu-who-opener');
     const eventsOpener = document.querySelector('.menu-events-opener');
+    const firstOpener = document.querySelector('.menu-first-opener');
+    const secondOpeners = document.querySelectorAll('.menu-second-opener');
+    const thirdOpeners = document.querySelectorAll('.menu-third-opener');
 
     const catalogMenu = document.querySelector('.menu--catalog');
     const whoMenu = document.querySelector('.menu--who');
     const eventsMenu = document.querySelector('.menu--events');
+    const firstMenu = document.querySelector('.menu--first');
+    const secondMenu = document.querySelector('.menu--second');
+    const thirdMenu = document.querySelector('.menu--third');
 
     // Функция для открытия меню
     function openMenu(menu) {
-        menu.classList.toggle('active'); // Добавляем класс для показа меню
+        menu.classList.toggle('menu--active'); // Добавляем класс для показа меню
     }
 
     // Открытие меню по клику на кнопку
@@ -28,10 +35,54 @@ document.addEventListener('DOMContentLoaded', function() {
         openMenu(eventsMenu);
     });
 
-    // Закрытие всех меню при клике вне их области
-    document.addEventListener('click', function() {
-        catalogMenu.classList.remove('active');
-        whoMenu.classList.remove('active');
-        eventsMenu.classList.remove('active');
+    firstOpener.addEventListener('click', function(event) {
+        event.stopPropagation();
+        openMenu(firstMenu);
     });
+
+    secondOpeners.forEach(opener => {
+      opener.addEventListener('click', function(event) {
+        event.stopPropagation();
+        openMenu(secondMenu);
+    });
+    });
+
+    thirdOpeners.forEach(opener => {
+      opener.addEventListener('click', function(event) {
+        event.stopPropagation();
+        openMenu(thirdMenu);
+    });
+    });
+
+     // Переменные для отслеживания скролла
+    let lastScrollTop = 0;
+
+    // Функция для обработки скролла
+    window.addEventListener('scroll', function() {
+        const scrollElements = document.querySelectorAll('.scroll-hide');
+        const headerBottom = document.querySelector('.header__bottom');
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        scrollElements.forEach(element => {
+            if (currentScroll > lastScrollTop) { // Скролл вниз
+                element.classList.add('scroll-hide--active');
+            } else { // Скролл вверх
+                if (currentScroll <= 0) {
+                    element.classList.remove('scroll-hide--active');
+                }
+            }
+        });
+
+        if (currentScroll > lastScrollTop) {
+          headerBottom.classList.add('header__bottom--scroll');
+        } else {
+          if (currentScroll <= 0) {
+              headerBottom.classList.remove('header__bottom--scroll');
+          }
+        }
+
+        // Обновляем последний скролл
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Для мобильных устройств или при прокрутке
+    });
+
 });
